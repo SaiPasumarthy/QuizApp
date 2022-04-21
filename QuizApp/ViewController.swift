90c0e08d4ccfbd00f8e730a47226d8273dbf5156
saipasumarthy <saipasumarthy009@gmail.com>
2022-04-21 18:35:48 +0530

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Sai Pasumarthy on 21/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
